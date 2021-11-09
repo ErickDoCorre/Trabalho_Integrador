1b9bd66f05f2883478105661783bf30d23876e35
MatetecoDW <93801097+MatetecoDW@users.noreply.github.com>
2021-11-08 21:05:19 -0300

    BANCO DE DADOS ORGANIZADO

---
+++ empresa foda.sql
@@ -0,0 +1,12 @@
+create database empresa;
+use empresa;
+ 
+create table agenda(
+idAgenda int primary key NOT NULL auto_increment,
+nomeCliente varchar(50),
+emailCliente varchar(50),
+telefoneCliente varchar(50),
+whatsCliente varchar (50),
+instaCliente varchar (50),
+twitterCliente varchar (50));
+
