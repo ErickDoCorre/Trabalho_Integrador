badb413c4d160e7653a2d0e046faf9ef7a832081
MatetecoDW <93801097+MatetecoDW@users.noreply.github.com>
2021-11-08 21:08:30 -0300

    BANCO DE DADOS ORGANIZADO(2) 0.2.0
    o banco de dados ta mais organizado, por favor, se tiver alguma coisa a acrescentar/criticar, estou aberto

--- clinica integradores.sql
+++ clinica integradores.sql
@@ -2,18 +2,19 @@ create database clinica;
 
 use clinica;
 
-create table paciente(idPaciente int not null auto_increment, nomePaciente varchar(50), sexoPaciente int, emergenciaPaciente varchar(20), enderecoPaciente varchar(20),
-    cpfCliente varchar (20), rgPaciente varchar(50), planoPaciente varchar(30), datanascPaciente varchar(20), estadoclinPaciente varchar(30), tiposangPaciente varchar (10),
-    remedPaciente varchar(20), responsaPaciente varchar(30), civilPaciente varchar (20));
+create table paciente(idPaciente int primary key not null auto_increment, nomePaciente varchar(20), idadePaciente varchar(20), datanascCliente varchar(30), sexoPaciente int, cpfPaciente varchar(20), rgPaciente varchar(20),
+tipoSanguineo varchar(10), estadoClinico varchar(200), medicacaoPaciente varchar(200), nomepaiPaciente varchar (20), nomemaePaciente varchar(30), planoPaciente varchar(30), enderecoPaciente varchar(50), estadocivilPaciente varchar(20));
 
-create table funcionario(idFuncionario int not null auto_increment, nomeFuncionario varchar (30), sexoFuncionario varchar(30), cpfFuncionario varchar(20), crmFuncionario varchar (30),
-emailFuncionario varchar(50), carteiraTrabalho varchar (30), tiposangFuncionario varchar(30), enderecoFuncionario varchar(30), civilFuncionario varchar (20), rgFuncionario varchar (30));
+create table funcionario(idFuncionario int primary key not null auto_increment, nomeFuncionário varchar(50), cpfFuncionario varchar(20), rgFuncionario varchar(20), emailFuncionario varchar(20), crmFuncionario varchar(20),
+tiposangueFuncionario varchar(10), carteiraTrabalho varchar(20), enderecoFuncionario varchar(50), estadocivilFuncionario varchar(20));
 
+/*id consulta, nome paciente, nome do funcionario, (perguntar para o douglas se o java aceita foreign key pra mudar o nome para id), data da consulta, horario da consulta, 
+tipo de consulta, valor da consulta*/
 
-create table consulta(idConsulta int not null auto_increment, nomeCliente varchar (20), nomeFuncionario varchar(30), dataConsulta varchar(30), horaConsulta varchar(30), tipoConsulta varchar(20),
-valorConsulta varchar (30));
-
-create table relatorio(idRelatorio int not null auto_increment, medicamentoCliente varchar (20), nomeMedico varchar(20), receitaCliente varchar (255), prontuarioConsulta varchar(255), 
-imcCliente varchar (20), dataConsulta varchar (20), atestadoCliente varchar(60));
+create table consulta(idConsulta int primary key not null auto_increment, nomePaciente varchar(20), nomeFuncionario varchar(30), dataConsulta varchar(30), 
+horarioConsulta varchar(30), tipoConsulta varchar(30), valorConsulta varchar(30));
+/*id relatorio, nomeFuncionario, nomePaciente, data consulta, imc paciente, atestado paciente, prontuario da consulta */
+create table relatorio(idRelatorio int primary key not null auto_increment, nomeFuncionario varchar(20), nomePaciente varchar(20), dataConsulta varchar(30), imcPaciente varchar(50), atestadoPaciente varchar(255),
+prontuarioConsulta varchar(255)) ;
 
 
